895ffd96b83d2b8f544dd5a2c921925802b27a07
Mimi Zhang <mimi02420@gmail.com>
2020-07-29 14:17:02 -0400

    Initial Commit

---
+++ TestFinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestFinalProject
+//
+//  Created by Mimi Zhang on 7/29/20.
+//  Copyright © 2020 Mimi Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
